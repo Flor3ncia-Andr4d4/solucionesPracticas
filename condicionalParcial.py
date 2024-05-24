3a77e1e14a99798edd55ffd55e49b541f51a05de
Flor3ncia-Andr4d4 <florenciacarolina031@gmail.com>
2024-05-24 20:32:33 -0300

    actualizando ejercicios

--- condicionalParcial.py
+++
@@ -1,8 +0,0 @@
-# Descripción: Verifica si un número ingresado por el usuario es positivo.
-
-# Entrada de datos
-numero = float(input("Ingrese un número: "))
-
-# Condicional parcial (solo el if) con expresión lógica simple
-if numero > 0:
-    print("El número ingresado es positivo.")
