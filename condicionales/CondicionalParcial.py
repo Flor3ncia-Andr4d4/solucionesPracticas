f2878b1af9c83ea2f7408a7e8bab30177544a9d0
Flor3ncia-Andr4d4 <florenciacarolina031@gmail.com>
2024-05-24 20:11:56 -0300

    Se comienza hacer ejercicio condicional parcial

---
+++ condicionales/CondicionalParcial.py
@@ -0,0 +1,8 @@
+# Descripción: Verifica si un número ingresado por el usuario es positivo.
+
+# Entrada de datos
+numero = float(input("Ingrese un número: "))
+
+# Condicional parcial (solo el if) con expresión lógica simple
+if numero > 0:
+    print("El número ingresado es positivo.")
